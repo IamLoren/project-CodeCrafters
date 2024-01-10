ac27d1f3b4b4bc510029fd2cc746590ad69ee1d2
Mary <32392607+MartFrida@users.noreply.github.com>
2024-01-11 00:32:27 +0100

    create chart statistic

---
+++ src/components/ChartStatistic/ChartStatistic.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const DoughnutContainerStyled = styled.div`
+width: 288px;
+height: 288px;
+position: relative;
+p{
+  position: absolute;
+  top: 120px;
+  left: 100px;
+}
+`
