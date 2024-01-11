7d844f3e0f834696ab6bc81fa2bb6ca602240a6f
Kirra-Potiekhina <kirrapotehina@gmail.com>
2024-01-11 22:11:56 +0200

    openmodalbtn add logic

---
+++ src/components/Home/HomeStyled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledHomeModalOpenBtn = styled.button`
+  position: absolute;
+  right: 40px;
+  bottom: 40px;
+  color: var(--white);
+  background-color: var(--button-gradient);
+  border-radius:50%;
+  outline: transparent;
+`;
