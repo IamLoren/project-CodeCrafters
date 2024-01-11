2c71641b96f97aa2dbe7e9f6d4f1ff91546e6bdc
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-01-12 01:05:22 +0200

    feature/ add currencyThunk in monobankAPI

--- src/components/MainLayout/Currency/Currency.jsx
+++ src/components/MainLayout/Currency/Currency.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   CurrencyTable,
   CurrencyTbody,
@@ -7,8 +7,20 @@ import {
   HigherRate,
   LowerRate,
 } from './CurrencyStyled';
+import { useDispatch, useSelector } from 'react-redux';
+import { currencySelector } from '../../../redux/selectors.js';
+import { currencyThunk } from '../../../redux/currency/operations.js';
 
 const Currency = () => {
+  const [currency, setCurrency] = useState([]);
+  const dispatch = useDispatch();
+  const todayCurrency = useSelector(currencySelector);
+
+  useEffect(() => {
+    dispatch(currencyThunk());
+    setCurrency(todayCurrency);
+  }, [dispatch, todayCurrency]);
+
   return (
     <>
       <CurrencyTable>
@@ -20,22 +32,37 @@ const Currency = () => {
           </tr>
         </CurrencyThead>
         <CurrencyTbody>
-          <tr>
-            <td>USD</td>
-            <td>27.55</td>
-            <td>27.65</td>
-          </tr>
-          <tr>
-            <td>EUR</td>
-            <td>30.00</td>
-            <td>30.10</td>
-          </tr>
+          {currency.map(item => {
+            console.log('item', item);
+            return (
+              <tr key={crypto.randomUUID()}>
+                <td>{item.currencyType}</td>
+                <td>{item.rateBuy}</td>
+                <td>{item.rateSell.toString().slice(0, -2)}</td>
+              </tr>
+            );
+          })}
         </CurrencyTbody>
       </CurrencyTable>
 
       <GraphBox>
-        <LowerRate>27.55</LowerRate>
-        <HigherRate>30.00</HigherRate>
+        {currency.map(item => {
+          if (item.currencyCodeA === 840) {
+            return (
+              <>
+                <LowerRate key={crypto.randomUUID()}>{item.rateBuy}</LowerRate>
+              </>
+            );
+          } else if (item.currencyCodeA === 978) {
+            return (
+              <>
+                <HigherRate key={crypto.randomUUID()}>
+                  {item.rateBuy}
+                </HigherRate>
+              </>
+            );
+          }
+        })}
       </GraphBox>
     </>
   );
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,3 @@
 export const selectIsLogged = state => state.authSlice.isLogged;
 export const modalIsOpen = state => state.transactionsSlice.modalIsOpen;
+export const currencySelector = state => state.currencySlice.data;
