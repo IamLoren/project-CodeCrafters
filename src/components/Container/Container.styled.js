7da455ae2e2d385db4f01a48d7256f98f41fcd6a
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-01-12 22:21:40 +0200

    fix container

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
-  width: 100%;
+  width: 1280px;
   margin: 0 auto;
   padding-left: 16px;
   padding-right: 16px;
