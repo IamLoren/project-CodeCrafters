0d91845227a65285715aff13bbb377794d510e31
Yulia Palamarchuk <83129717+YuliaMiha@users.noreply.github.com>
2024-01-13 11:27:25 +0200

    fix store.js

--- src/components/TransactionTable/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionTable/TransactionsList/TransactionsList.jsx
@@ -14,43 +14,6 @@ import {selectTransactionsList} from '../../../redux/selectors'
 
 
 const TransactionsList = () => {
-  // const [transactions /*, setTransactions*/] = useState/[
-  //   {
-  //     date: "04.01.24",
-  //     type: "-",
-  //     category: "Other",
-  //     comment: "Gift for your wife",
-  //     amount: 300.00,
-  //   },
-  //   {
-  //     date: "05.01.23",
-  //     type: "+",
-  //     category: "Income",
-  //     comment: "January bonus",
-  //     amount: 8000.00,
-  //   },
-  //   {
-  //     date: "07.01.23",
-  //     type: "-",
-  //     category: "Car",
-  //     comment: "Oil",
-  //     amount: 1000.00,
-  //   },
-  //   {
-  //     date: "07.01.23",
-  //     type: "-",
-  //     category: "Products",
-  //     comment: "Vegetables for the week",
-  //     amount: 280.00,
-  //   },
-  //   {
-  //     date: "07.01.23",
-  //     type: "+",
-  //     category: "Income",
-  //     comment: "Gift",
-  //     amount: 1000.00,
-  //   },
-  // ]);
 
   const dispatch = useDispatch();
   const transactions = useSelector(selectTransactionsList); 
@@ -76,7 +39,7 @@ const TransactionsList = () => {
       <TransactionTbody>
           {transactions.length === 0 ? (
             <tr>
-              <td colSpan="6">Ви ще не маєте власних транзакцій</td>
+              <td colSpan="6">You don't have any transactions of your own yet</td>
             </tr>
           ) : (
             transactions.map((transaction) => (
