cdbd977fab218a1f19ac318c56b39f0bdda0b8ff
IamLoren <94172173+IamLoren@users.noreply.github.com>
2024-01-14 18:18:45 +0200

    Update StatisticsTable.jsx

--- src/components/Statistics/StatisticsTable.jsx
+++ src/components/Statistics/StatisticsTable.jsx
@@ -146,15 +146,15 @@ const StatisticsTable = () => {
     </WrapCategories>
   );
 };
-const coloredCategoriesMap = new Map([
-  ['Main expenses', 'rgba(254, 208, 87, 1)'],
-  ['Products', 'rgba(255, 216, 208, 1)'],
-  ['Car', 'rgba(253, 148, 152, 1)'],
-  ['Self care', 'rgba(197, 186, 255, 1)'],
-  ['Child care', 'rgba(110, 120, 232, 1)'],
-  ['Household products', 'rgba(74, 86, 226, 1)'],
-  ['Education', 'rgba(129, 225, 255, 1)'],
-  ['Leisure', 'rgba(36, 204, 167, 1)'],
-  ['Other expenses', 'rgba(0, 173, 132, 1)'],
-]);
+// const coloredCategoriesMap = new Map([
+//   ['Main expenses', 'rgba(254, 208, 87, 1)'],
+//   ['Products', 'rgba(255, 216, 208, 1)'],
+//   ['Car', 'rgba(253, 148, 152, 1)'],
+//   ['Self care', 'rgba(197, 186, 255, 1)'],
+//   ['Child care', 'rgba(110, 120, 232, 1)'],
+//   ['Household products', 'rgba(74, 86, 226, 1)'],
+//   ['Education', 'rgba(129, 225, 255, 1)'],
+//   ['Leisure', 'rgba(36, 204, 167, 1)'],
+//   ['Other expenses', 'rgba(0, 173, 132, 1)'],
+// ]);
 export default StatisticsTable;
